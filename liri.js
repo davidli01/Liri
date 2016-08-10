1328082ff6b264cc8ea0985dee79f155814f2f11
David Li <jdavidli01@outlook.com>
2016-08-10 15:15:11 -0400

    initial commit and file updates

---
+++ liri.js
@@ -0,0 +1,9 @@
+var keys = require('./twitterKeys');
+
+
+var tweets = process.argv[2];
+var song = process.argv[3];
+var movie = process.argv[4];
+var action = process.argv[5];
+
+console.log(process.argv)
